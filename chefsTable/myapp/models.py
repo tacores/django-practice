2e994d490ed8d7344716882384916a0073e98d22
tacores <buttix@gmail.com>
2024-04-30 09:05:04 +0900

    end of w3-1

--- chefsTable/myapp/models.py
+++ chefsTable/myapp/models.py
@@ -1,6 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class DrinksCategory(models.Model):
+    category_name = models.CharField(max_length=200)
+
 class Drinks(models.Model):
     drink_name = models.CharField(max_length=200)
     price = models.IntegerField()
+    category = models.ForeignKey(DrinksCategory, on_delete=models.PROTECT, default=None)
